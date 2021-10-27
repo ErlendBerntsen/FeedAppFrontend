08868a99f3b6b86e343353001f7243411c4e73d4
Simen <simencarstensen@gmail.com>
2021-10-28 00:30:30 +0200

    Users can now be registered on the userCreationPage

--- src/UserCreationPage.js
+++ src/UserCreationPage.js
@@ -1,17 +1,19 @@
 import React from "react";
+import axios from "axios";
+
 
 class UserCreationPage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             fields: {},
-            errors: {}
-          }
-
+            errors: {},
+        }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
 
+    //update state with input in textfields
     handleChange(event) {
         const target = event.target;
         let fields = this.state.fields
@@ -21,30 +23,41 @@ class UserCreationPage extends React.Component {
         });
     }
 
+    //performs a post request with current fields when pressing submit button
     handleSubmit(event) {
-        
+        let fields = this.state.fields;
+        let errors = this.state.errors;
         if (this.handleValidation()) {
-            //const url = "http://localhost:8080/users"
-            alert('Sent form');
+            let success = true
+            const url = "http://localhost:8080/users"
+            const body = { username: fields["userName"], password: fields["password1"], userType: "REGULAR" }
+            axios.post(url, body)
+                .then(response => response.data)
+                .catch(error => {
+                    errors["response"] = error.message 
+                    this.setState({ errors: errors });
+                    success = false
+                });
+            if (success) {
+                alert('User created successfully');
+            }
         }
-        else {
-            alert('An error occured');
-        }
-        
         event.preventDefault();
     }
 
+    //returns true if all input fields are valid
     handleValidation() {
         let fields = this.state.fields;
         let errors = {};
         let isValid = true;
-        if(!fields["userName"]) {
+        //TODO: check if username exists
+        if (!fields["userName"]) {
             isValid = false
             errors["userName"] = "Username cannot be empty"
         }
-        if(!fields["password1"]) {
+        if (fields["password1"].length < 8) {
             isValid = false
-            errors["password1"] = "Password cannot be empty"
+            errors["password1"] = "Password must be at least 8 characters"
         }
         if (fields["password1"] !== fields["password2"]) {
             isValid = false
@@ -75,6 +88,8 @@ class UserCreationPage extends React.Component {
                     <span style={{ color: "red" }}>{this.state.errors["password2"]}</span>
                     <br />
                     <input type="submit" value="Submit" />
+                    <br />
+                    <span style={{ color: "red" }}>{this.state.errors["response"]}</span>
                 </form>
             </div>
         );
