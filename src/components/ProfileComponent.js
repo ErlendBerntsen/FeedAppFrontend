5643beef72c157a048b5e6a496b25787622c7098
Simen <simencarstensen@gmail.com>
2021-10-31 12:25:37 +0100

    Profilepage gets data and displays it

--- src/components/ProfileComponent.js
+++ src/components/ProfileComponent.js
@@ -5,12 +5,11 @@ import axios from "axios";
 class ProfileComponent extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       redirect: null,
       userReady: false,
       currentUser: { username: '', id: '' },
-      content: '',
+      content: {},
       error: ''
     };
   }
@@ -19,35 +18,33 @@ class ProfileComponent extends Component {
   componentDidMount() {
     const currentUser = JSON.parse(localStorage.getItem('user'));
     if (!currentUser) this.setState({ redirect: "/" }); //redirect to home if no current user
-    this.setState({ currentUser: currentUser, userReady: true })
 
-    //collect data for this user
-    if (this.state.redirect) {
+    else {  //collect data for this user
+      this.setState({ currentUser: currentUser, userReady: true })
       const url = "http://localhost:8080/users/" + currentUser.id
       axios.get(url) //TODO: change when authtoken is needed
         .then(response => {
-          this.setState({ content: response.data }) //TODO: currently not working, content is empty
+          this.setState({ content: response.data })
         })
         .catch(err => {
           this.setState({ error: err.message });
         });
     }
-  }
 
+  }
 
   render() {
     if (this.state.redirect) {
       return <Redirect to={this.state.redirect} />
     }
     const { currentUser } = this.state;
+    const { content } = this.state
+    
 
     return (
       <div>
         <h2><strong>{currentUser.username}</strong> Profile</h2>
-        <p>
-          <strong>Id:</strong>{" "}
-          {currentUser.id}
-        </p>
+        {JSON.stringify(content)}
         <p>
           <span style={{ color: "red" }}>{this.state.error}</span>
         </p>
