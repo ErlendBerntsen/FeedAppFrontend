6eb91d65cacea0b88dc73fd36ae833721d60101e
Simen <simencarstensen@gmail.com>
2021-11-02 00:40:21 +0100

    logged in users can create a poll

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -27,6 +27,7 @@ class AuthService {
     logout() {
         localStorage.removeItem("user")
         alert("Logged out")
+        window.location.reload(false);
     }
 }
 
--- src/services/PollService.js
+++ src/services/PollService.js
@@ -19,17 +19,14 @@ class PollService {
     }
 
     createPoll(question, votingStart, votingEnd, isPrivate) {
-        const creatorId = JSON.parse(localStorage.getItem("user")).id //get id of current user
-        if (!creatorId) {
-            //TODO: don't know if this check is necessary. add error handling if it is. 
-        }
-        const body = { creatorId: creatorId, question: question, votingStart: votingStart, votingEnd: votingEnd, isPrivate: isPrivate }
+        const currentUser = JSON.parse(localStorage.getItem('user'));
+        const body = { creatorId: currentUser.id, question: question, votingStart: votingStart, votingEnd: votingEnd, isPrivate: isPrivate }
         return axios.post(url, body)
     }
 
     addVote(pollId, voterId, optionChosen, voteType) {
         const body = { pollId: pollId, voterId: voterId, optionChosen: optionChosen, voteType: voteType }
-        return axios.post(url + pollId + "/votes")
+        return axios.post(url + pollId + "/votes", body)
     }
 
     getAllVotes(pollId) {
